2547bba18fedda092c582c7f12f319da8e022e8a
Shivam Gupta <shivamgupta02005@gmail.com>
2024-01-30 18:56:49 +0530

    Make Operations using on-screen keyboard work

--- src/App.js
+++ src/App.js
@@ -1,37 +1,112 @@
+import { useState } from 'react';
 import './index.css';
+
 function App() {
+  const [screenValue, setScreenValue] = useState('');
+  const operators = ['%', '/', '+', '-', '*'];
+
+  const operator = screenValue
+    .split('')
+    .map((letter) => (operators.includes(letter) ? letter : ''))
+    .find((operator) => operator !== '');
+
+  const num1 = screenValue.split(operator)[0];
+  const num2 = screenValue.split(operator)[1];
+  console.log(operator, num1, num2);
+
+  function handleOperationClick(operation) {
+    if (!screenValue || ['%', '/', '+', '-'].includes(screenValue.slice(-1)))
+      return;
+    if (operator) {
+      const result = performOperation();
+      setScreenValue(result);
+    }
+    setScreenValue((value) => value + operation);
+  }
+
+  function performOperation() {
+    let result;
+    switch (operator) {
+      case '%':
+        result = +num1 % +num2;
+        break;
+      case '+':
+        result = +num1 + +num2;
+        break;
+      case '-':
+        result = +num1 - +num2;
+        break;
+      case '*':
+        result = +num1 * +num2;
+        break;
+      case '/':
+        result = +num1 / +num2;
+        break;
+      default:
+        console.log('invalid operator');
+        break;
+    }
+    return result.toString();
+  }
+
+  function handleEqualsClick() {
+    if (!num1 || !num2 || !operator) return;
+    const result = performOperation();
+    setScreenValue(result);
+  }
+
+  function handleDecimalClick() {
+    if (!screenValue || screenValue.slice(-1) === '.') return;
+    setScreenValue((value) => value + '.');
+  }
+
   return (
     <>
-      <Screen />
+      <Screen screenValue={screenValue} />
       <KeyBoard>
-        <AllClearButton />
-        <ClearButton />
-        <OperationButton operation={'%'} />
-        <OperationButton operation={'/'} />
-        <Digit digit={7} />
-        <Digit digit={8} />
-        <Digit digit={9} />
-        <OperationButton operation={'+'} />
-        <Digit digit={4} />
-        <Digit digit={5} />
-        <Digit digit={6} />
-        <OperationButton operation={'-'} />
-        <Digit digit={1} />
-        <Digit digit={2} />
-        <Digit digit={3} />
-        <OperationButton operation={'*'} />
-        <Digit digit={0} />
-        <Decimalbutton />
-        <EqualsButton />
+        <AllClearButton setScreenValue={setScreenValue} />
+        <ClearButton setScreenValue={setScreenValue} />
+        <OperationButton
+          operation={'%'}
+          onOperationClick={handleOperationClick}
+        />
+        <OperationButton
+          operation={'/'}
+          onOperationClick={handleOperationClick}
+        />
+        <Digit digit={7} setScreenValue={setScreenValue} />
+        <Digit digit={8} setScreenValue={setScreenValue} />
+        <Digit digit={9} setScreenValue={setScreenValue} />
+        <OperationButton
+          operation={'+'}
+          onOperationClick={handleOperationClick}
+        />
+        <Digit digit={4} setScreenValue={setScreenValue} />
+        <Digit digit={5} setScreenValue={setScreenValue} />
+        <Digit digit={6} setScreenValue={setScreenValue} />
+        <OperationButton
+          operation={'-'}
+          onOperationClick={handleOperationClick}
+        />
+        <Digit digit={1} setScreenValue={setScreenValue} />
+        <Digit digit={2} setScreenValue={setScreenValue} />
+        <Digit digit={3} setScreenValue={setScreenValue} />
+        <OperationButton
+          operation={'*'}
+          onOperationClick={handleOperationClick}
+        />
+        <Digit digit={0} setScreenValue={setScreenValue} />
+        <Decimalbutton onDecimalClick={handleDecimalClick} />
+        <EqualsButton onEqualsClick={handleEqualsClick} />
       </KeyBoard>
     </>
   );
 }
 
-function Screen() {
+function Screen({ screenValue }) {
   return (
     <div className="screen">
-      <input type="text" id="screen-input" />
+      <input type="text" id="screen-input" value={screenValue} />
     </div>
   );
 }
@@ -40,28 +115,58 @@ function KeyBoard({ children }) {
   return <div className="button-grid">{children}</div>;
 }
 
-function OperationButton({ operation }) {
-  return <button className="operators">{operation}</button>;
+function OperationButton({ operation, onOperationClick }) {
+  return (
+    <button className="operators" onClick={() => onOperationClick(operation)}>
+      {operation}
+    </button>
+  );
 }
 
-function Digit({ digit }) {
-  return <button className="digits">{digit}</button>;
+function Digit({ digit, setScreenValue }) {
+  function handleDigitClick() {
+    setScreenValue((value) => value + digit);
+  }
+  return (
+    <button className="digits" onClick={handleDigitClick}>
+      {digit}
+    </button>
+  );
 }
 
-function AllClearButton() {
-  return <button className="all-clear">AC</button>;
+function AllClearButton({ setScreenValue }) {
+  return (
+    <button className="all-clear" onClick={() => setScreenValue('')}>
+      AC
+    </button>
+  );
 }
 
-function ClearButton() {
-  return <button className="clear">C</button>;
+function ClearButton({ setScreenValue }) {
+  function handleClearClick() {
+    setScreenValue((value) => value.slice(0, value.length - 1));
+  }
+  return (
+    <button className="clear" onClick={handleClearClick}>
+      C
+    </button>
+  );
 }
 
-function Decimalbutton() {
-  return <button className="decimal">.</button>;
+function Decimalbutton({ onDecimalClick }) {
+  return (
+    <button className="decimal" onClick={onDecimalClick}>
+      .
+    </button>
+  );
 }
 
-function EqualsButton() {
-  return <button className="equals">=</button>;
+function EqualsButton({ onEqualsClick }) {
+  return (
+    <button className="equals" onClick={onEqualsClick}>
+      =
+    </button>
+  );
 }
 
 export default App;
